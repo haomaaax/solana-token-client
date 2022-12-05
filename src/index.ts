5fe13aadd9cc9e71aff0ff1b6b72c548667bbeda
maaax.eth <maxyay5566@gmail.com>
2022-12-05 23:03:15 +0800

    add updateTokenMetadata function

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import {
 import {
   DataV2,
   createCreateMetadataAccountV2Instruction,
-  //createUpdateMetadataAccountV2Instruction,
+  createUpdateMetadataAccountV2Instruction,
 } from "@metaplex-foundation/mpl-token-metadata"
 import * as fs from "fs"
 
@@ -78,6 +78,79 @@ async function createTokenMetadata(
     )
   )
 
+  // send transaction
+  const transactionSignature = await web3.sendAndConfirmTransaction(
+    connection,
+    transaction,
+    [user]
+  )
+
+  console.log(
+    `Create Metadata Account: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+  )
+}
+
+async function updateTokenMetadata(
+  connection: web3.Connection,
+  metaplex: Metaplex,
+  mint: web3.PublicKey,
+  user: web3.Keypair,
+  name: string,
+  symbol: string,
+  description: string
+) {
+  // file to buffer
+  const buffer = fs.readFileSync("assets/cool.png")
+
+  // buffer to metaplex file
+  const file = toMetaplexFile(buffer, "cool.png")
+
+  // upload image and get image uri
+  const imageUri = await metaplex.storage().upload(file)
+  console.log("image uri:", imageUri)
+
+  // upload metadata and get metadata uri (off chain metadata)
+  const { uri } = await metaplex
+    .nfts()
+    .uploadMetadata({
+      name: name,
+      description: description,
+      image: imageUri,
+    })
+ 
+  console.log("metadata uri:", uri)
+
+  // get metadata account address
+  const metadataPDA = metaplex.nfts().pdas().metadata({mint})
+
+  // onchain metadata format
+  const tokenMetadata = {
+    name: name,
+    symbol: symbol,
+    uri: uri,
+    sellerFeeBasisPoints: 0,
+    creators: null,
+    collection: null,
+    uses: null,
+  } as DataV2
+
+  // transaction to update metadata account
+  const transaction = new web3.Transaction().add(
+    createUpdateMetadataAccountV2Instruction(
+      {
+        metadata: metadataPDA,
+        updateAuthority: user.publicKey,
+      },
+      {
+        updateMetadataAccountArgsV2: {
+          data: tokenMetadata,
+          updateAuthority: user.publicKey,
+          primarySaleHappened: true,
+          isMutable: true,
+        },
+      }
+    )
+  )
   // send transaction
   const transactionSignature = await web3.sendAndConfirmTransaction(
     connection,
@@ -233,6 +306,7 @@ async function main() {
     connection,
     user,
     mint,
+    // This should be your dev wallet account address, which holds your token mint
     new web3.PublicKey("4Ao1XPFTE7rnuyibnKqoNWQydFyXRub5sUUSYZFz4qPK")
   )
 
